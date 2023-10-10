29357e797f95de0e6a0843b3e179a0c674a9e0af
Rehan Ali <rehannali@programmer.net>
2023-10-10 14:13:19 +0500

    Update colorscheme.lua

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,4 +1,7 @@
 vim.o.termguicolors = true
+-- vim.o.background = "dark"
 -- vim.cmd [[ colorscheme dracula ]]
-vim.g.material_style = "darker"
-vim.cmd 'colorscheme material'
+-- vim.g.material_style = "darker"
+-- vim.cmd 'colorscheme material'
+-- Lua initialization file
+vim.cmd [[colorscheme moonfly]]
